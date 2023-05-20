a2c827b88423a26be5e14006bf146f52dd0a2d82
Sanela <saneladovlete@yahoo.com>
2023-05-20 14:46:35 +0300

    hello

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./index.css";
+import "./App.css";
+
+function App() {
+  return <p className="text-3xl font-bold underline text-red-500">hi</p>;
+}
+
+export default App;
